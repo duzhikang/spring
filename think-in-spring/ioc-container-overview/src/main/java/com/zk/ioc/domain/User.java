d5332deb66cd3f565318288b80fe7d4ab30bc56f
duzhikang <duzhikang@hiram.cn>
2020-01-14 22:39:42 +0800

    first commit

---
+++ think-in-spring/ioc-container-overview/src/main/java/com/zk/ioc/domain/User.java
@@ -0,0 +1,41 @@
+package com.zk.ioc.domain;/**
+ * Created by dzk on 2020/1/14.
+ */
+
+/**
+ * @ClassName User
+ * @Description
+ * @Author dzk
+ * @Version v1
+ * @Date 2020/1/14
+ **/
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ think-in-spring/ioc-container-overview/src/main/java/com/zk/ioc/lookup/DependencyLookupDemo.java
@@ -0,0 +1,27 @@
+package com.zk.ioc.lookup;/**
+ * Created by dzk on 2020/1/14.
+ */
+
+import com.zk.ioc.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ClassName DependencyLookupDemo
+ * @Description
+ * @Author dzk
+ * @Version v1
+ * @Date 2020/1/14
+ **/
+public class DependencyLookupDemo {
+
+    public static void main(String[] args) {
+
+        // 配置 XML 配置文件
+        // 启动 Spring 应用上下文
+        // IOC容器的最基本形式，并提供了IOC容器应遵守的的最基本的接口，也就是Spring IOC所遵守的最底层和最基本的编程规
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+        User user = (User) beanFactory.getBean("user");
+        System.out.println(user);
+    }
+}
